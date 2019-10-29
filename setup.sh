1cf7ae6f6a0d240605b970efa3c0faf60b9d5eab
Cameron McCallie <cam@cameo.com>
2019-10-29 09:28:19 -0700

    setup for zsh

--- setup.sh
+++ setup.sh
@@ -5,5 +5,14 @@ cp -f ./gitconfig ~/.gitconfig
 # After running, run :source in a vim terminal
 ln -s ./vimrc ~/.vimrc
 
+# install wget
+brew install wget
+
+# install python
+brew install python
+sudo easy_install pip
+
+# oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 echo Done
